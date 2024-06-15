32840b8a23982afb54ffd568d51df5ee025dd056
Valliappan2125 <valliappan1813@gmail.com>
2024-06-15 10:26:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showNavbar(){
+    document.getElementById("side-navbar").style.left="0";
+    document.getElementById("menu-bar-toggle").style.display="none";
+}
+
+function closeNavbar(){
+    document.getElementById("side-navbar").style.left="-50%";
+    document.getElementById("menu-bar-toggle").style.display="block";
+}
